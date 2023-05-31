6c2479290eeea8a3e2d07c8f048de750d516a6c0
LidorLazar <lidorlazar33@gmail.com>
2023-06-01 02:58:50 +0300

    Add contact + send email from form

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () =>{
+    return(
+        <footer>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <h3> © האתר עוצב ונבנה על ידי <a href='https://www.linkedin.com/in/lidor-lazar/' className={'hover:underline'}>לידור לזר</a></h3>
+        </footer>
+    )
+}
+
+
+export default Footer;
